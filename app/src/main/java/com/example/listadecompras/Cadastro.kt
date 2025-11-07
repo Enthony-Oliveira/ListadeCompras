ea1c70bdf295d898923d9cbf3b7750c136d1e03f
Enthony-Oliveira <oliveiraenthony@gmail.com>
2025-11-06 21:58:53 -0300

    Initial commit

--- app/src/main/java/com/example/listadecompras/Cadastro.kt
+++ app/src/main/java/com/example/listadecompras/Cadastro.kt
@@ -1,20 +1,29 @@
 package com.example.listadecompras
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 class Cadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_main)
+
+//        val adapter_produtos = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+//        val list_view = findViewById<ListView>(R.id.list_view_produtos)
+//
+//        list_view.adapter = adapter_produtos
+//
+//        val editText = findViewById<EditText>(R.id.NomeProduto)
+//        val botao = findViewById<Button>(R.id.btn_inserir)
+//
+//        btn_inserir.setOnClickerListener{
+//            val nome_produto = editText.text.toString()
+//            adapter_produtos.add(nome_produto)
+//        }
     }
 }
