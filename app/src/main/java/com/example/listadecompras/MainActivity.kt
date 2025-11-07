66d6d587a53868b8245a6c804d76cef62cebb744
Enthony-Oliveira <oliveiraenthony@gmail.com>
2025-11-06 21:58:53 -0300

    Initial commit

--- app/src/main/java/com/example/listadecompras/MainActivity.kt
+++ app/src/main/java/com/example/listadecompras/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.listadecompras
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,33 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-        val adapter_produtos = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
-
-        val nomeProdutoEditText = findViewById<EditText>(R.id.NomeProduto)
-        val qtdItensEditText = findViewById<EditText>(R.id.Qtd_itens)
-        val valorEditText = findViewById<EditText>(R.id.txt_valor)
-        val botaoInserir = findViewById<Button>(R.id.btn_inserir)
-
-        botaoInserir.setOnClickListener{
-            val nome = nomeProdutoEditText.text.toString().trim()
-            val quantidade = qtdItensEditText.text.toString().trim()
-            val valor = valorEditText.text.toString().trim()
-
-            if (nome.isNotBlank()) {
-                val produtoFormatado = "Produto: $nome | Qtd: $quantidade | R$: $valor"
-
-                adapter_produtos.add(produtoFormatado)
-                adapter_produtos.notifyDataSetChanged()
-
-                nomeProdutoEditText.text.clear()
-                qtdItensEditText.text.clear()
-                valorEditText.text.clear()
-                nomeProdutoEditText.requestFocus()
-            } else {
-                Toast.makeText(this, "O Nome do Produto é obrigatório!", Toast.LENGTH_SHORT).show()
-            }
+        val btn_inserir = findViewById<Button>(R.id.btn_inserir)
+        btn_inserir.setOnClickListener {
+            val intent = Intent(this, Cadastro::class.java)
+            startActivity(intent)
         }
     }
 }
